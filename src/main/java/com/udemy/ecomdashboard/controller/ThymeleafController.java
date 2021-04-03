4eb811bb1a17743e4bc5a324585516c74aae4e58
Nina Nguyen <nguyenn4@seattleu.edu>
2021-04-02 22:23:48 -0700

    Working dashboard

---
+++ src/main/java/com/udemy/ecomdashboard/controller/ThymeleafController.java
@@ -0,0 +1,25 @@
+package com.udemy.ecomdashboard.controller;
+
+import com.udemy.ecomdashboard.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("/dashboard")
+    public String getDashDetail(Model model){
+
+        /*Populate Company Revenue Data*/
+        model.addAttribute("cr", dashboardService.getTodayRevenueDash());
+
+        //Populate the Employees info
+        model.addAttribute("ei", dashboardService.getAllEmployee());
+        return "/dashboard";
+    }
+}
